93ee95a7887c89d88ca24a395dacb5eba5c7f22b
Akarshan Kapoor <95586108+CodingWarrior33@users.noreply.github.com>
2023-07-07 13:00:26 +0530

    Update files

--- approach2.py
+++ approach2.py
@@ -31,7 +31,6 @@ nltk.download('stopwords')
 sw_nltk = stopwords.words('english')
 
 def remove_html_tags(text):
-    """Remove html tags from a string"""
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
